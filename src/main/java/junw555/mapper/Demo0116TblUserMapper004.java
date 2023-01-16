3304ec0e72e7edbe2b56c65f2df93f1f7fbdc35f
junw <45444154+wo1261931780@users.noreply.github.com>
2023-01-16 20:56:08 +0800

    mybatisPLus-day1-finish

---
+++ src/main/java/junw555/mapper/Demo0116TblUserMapper004.java
@@ -0,0 +1,18 @@
+package junw555.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import junw555.pojo.Demo0116User003;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-spring.github.io
+ * Package:mapper
+ *
+ * @author liujiajun_junw
+ * @Date 2023-01-13-15  星期一
+ * @description
+ */
+@Mapper
+public interface Demo0116TblUserMapper004 extends BaseMapper<Demo0116User003> {
+}
