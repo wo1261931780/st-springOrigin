5b8f1bc4765a83179f37ac247b8491ca4c17e94a
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 21:16:27 +0800

    3

---
+++ module002/src/main/java/junw555/dao/DemoDao001.java
@@ -0,0 +1,13 @@
+package junw555.dao;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:st-spring.github.io
+ * Package:junw555.dao
+ *
+ * @author liujiajun_junw
+ * @Date 2023-01-13-15  星期一
+ * @description
+ */
+public class DemoDao001 {
+}
---
+++ src/main/java/wo1261931780/stspringOrigin/config/MybatisPlusConfiguration.java
@@ -0,0 +1,26 @@
+package junw555.config;
+
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Intellij IDEA.
+ * Project:springBootDemo0116
+ * Package:com.example.springbootdemo0116.config
+ *
+ * @author liujiajun_junw
+ * @Date 2023-01-19-26  星期一
+ * @description
+ */
+// @Configuration
+// @MapperScan("junw555.mapper")
+public class MybatisPlusConfiguration {
+	// 配置分页插件
+
+	@Bean
+	public PaginationInnerInterceptor paginationInnerInterceptor() {
+		return new PaginationInnerInterceptor();
+	}
+}
