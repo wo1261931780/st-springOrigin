60c211c528eec2fdfdd5b108db4895cd3b443e2d
junw <45444154+wo1261931780@users.noreply.github.com>
2023-02-26 21:15:22 +0800

    2

---
+++ src/main/java/wo1261931780/stspringOrigin/StSpringOriginApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.stspringOrigin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StSpringOriginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StSpringOriginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/stspringOrigin/StSpringOriginApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.stspringOrigin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StSpringOriginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
